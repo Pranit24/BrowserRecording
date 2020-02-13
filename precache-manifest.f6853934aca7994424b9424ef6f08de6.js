6a7b0c200c39e2b82ba434d2e01c61117f4cd582
Pranit <pranit.shetty@gmail.com>
2020-02-13 15:28:54 -0500

    Updates

---
+++ precache-manifest.f6853934aca7994424b9424ef6f08de6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2252c73144906abd4ea4171529438ed1",
+    "url": "/BrowserRecording/index.html"
+  },
+  {
+    "revision": "7dcd01c86bededbd83a5",
+    "url": "/BrowserRecording/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "3fdf7207c3e12705002f",
+    "url": "/BrowserRecording/static/css/main.2e3c917d.chunk.css"
+  },
+  {
+    "revision": "7dcd01c86bededbd83a5",
+    "url": "/BrowserRecording/static/js/2.2519b51e.chunk.js"
+  },
+  {
+    "revision": "ac0b00e242766df573c6b02ebd0322fa",
+    "url": "/BrowserRecording/static/js/2.2519b51e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fdf7207c3e12705002f",
+    "url": "/BrowserRecording/static/js/main.a3a0a79a.chunk.js"
+  },
+  {
+    "revision": "0d9b864847a8ab6b71ce",
+    "url": "/BrowserRecording/static/js/runtime-main.7e7c463c.js"
+  }
+]);
